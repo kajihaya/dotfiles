2dfd6381892fd618d3b3c06aa8075a4769bd06ec
Hayato Kajiyama <kajiyamah61@COAMAC15093117.local>
2018-06-08 16:27:30 +0900

    update: addapt gcloud

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,3 +16,9 @@ export SVN_EDITOR='vim'
 #-----------
 balias g git
 balias e emacsclient
+## gcloud
+balias gcp 'gcloud compute copy-files'
+balias glist 'gcloud compute instances list'
+balias gsh 'gcloud compute ssh'
+balias gup 'gcloud compute instances start'
+balias gdown 'gcloud compute instances stop'
