54873886f787655fb429751cc3d7eba7ced47a9d
Hayato Kajiyama <kajiyamah61@COAMAC17000052.local>
2018-11-15 13:43:51 +0900

    update: fish and git settings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,24 +1,36 @@
-#-----------
+# general settings
+#------------------
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+
+
 # key bindings
 #-----------
 function fish_user_key_bindings
   bind \cr 'peco_select_history (commandline -b)'
 end
 
-#-----------
 # editor
 #-----------
 export EDITOR='vim'
 export SVN_EDITOR='vim'
 
-#-----------
+
 # alias
 #-----------
+## misc
 balias g git
 balias e emacsclient
+balias l exa
+
 ## gcloud
 balias gcp 'gcloud compute copy-files'
 balias glist 'gcloud compute instances list'
 balias gsh 'gcloud compute ssh'
 balias gup 'gcloud compute instances start'
 balias gdown 'gcloud compute instances stop'
+
+## docker
+balias d 'docker'
+balias dc 'docker container'
+
+
