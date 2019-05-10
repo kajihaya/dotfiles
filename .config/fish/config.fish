bf3242628974e895fd3a8b2ea8c1b337d35283ef
hyakt <hyakt0@gmail.com>
2019-05-10 22:06:22 +0900

    yadd: pub path (dart)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,9 +2,11 @@
 #------------------
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/ruby/bin" $fish_user_paths
-set -g fish_user_paths /usr/local/lib/ruby/gems/2.6.0/bin $fish_user_paths
+set -g fish_user_paths "/usr/local/lib/ruby/gems/2.6.0/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 set -g fish_user_paths "$HOME/git/github.com/flutter/flutter/bin" $fish_user_paths
+set -g fish_user_paths "$HOME/.pub-cache/bin" $fish_user_paths
+
 
 set GHQ_SELECTOR peco
 
