7607f1420b2f1f2e9990d3c52545e71765e242da
hyakt <hyakt0@gmail.com>
2021-03-18 00:46:01 +0900

    update: fish keybindings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -41,7 +41,7 @@ set GHQ_SELECTOR peco
 #-----------
 function fish_user_key_bindings
     bind \cr 'peco_select_history (commandline -b)'
-    bind \cb peco_checkout_git_branch
+    bind \cx peco_checkout_git_branch
 end
 
 # editor
